fa4f849e3a0b74d0f6894648fce8a4a648a8eecb
Jess Helmer <jhelmer3@gatech.edu>
2025-09-26 10:50:47 -0400

    Adding Lab 6 Slides

--- labs/Lab 5 - Intermediate R/lab 5 scratch.R
+++ labs/Lab 5 - Intermediate R/lab 5 scratch.R
@@ -1,36 +1,50 @@
 
+#install.packages("psych")
+install.packages("tidyverse")
+
 library(tidyverse)
 
 pulse_data_raw <- rio::import("http://www.statsci.org/data/oz/ms212.txt") 
 
-head(pulse_data)
-summary(pulse_data)
-str(pulse_data)
-psych::describe(pulse_data)
+head(pulse_data_raw)
+summary(pulse_data_raw)
+str(pulse_data_raw)
+psych::describe(pulse_data_raw)
 
 pulse_data <- pulse_data_raw |>
   mutate(PulseDiff = Pulse2 - Pulse1,
-         Smokes = factor(Smokes, levels = c(1, 2), labels = c("Yes", "No")),
-         Ran = factor(Ran, levels = c(1, 2), labels = c("Ran", "Sat"))) |>
-  select(Smokes, Ran, Pulse1, Pulse2, PulseDiff)
+         SmokesFct = factor(Smokes, levels = c(1, 2), labels = c("Yes", "No")),
+         RanFct = factor(Ran, levels = c(1, 2), labels = c("Ran", "Sat")),
+         ExerciseFct = factor(Exercise, levels = c(1, 2, 3), labels = c("High", "Moderate", "Low"))) |>
+  select(SmokesFct, RanFct, ExerciseFct, PulseDiff, Pulse1, Pulse2)
 
 
 pulse_data |>
-  summarize(.by = Ran,
+  summarize(.by = RanFct,
             PulseDiffMean = mean(PulseDiff, na.rm = T))
 
 pulse_data |>
-  summarize(.by = c(Ran, Smokes),
+  summarize(.by = c(RanFct, SmokesFct),
             PulseDiffMean = mean(PulseDiff, na.rm = T))
 
 
-ggplot(data = pulse_data,
-       mapping = aes(x = Pulse1, y = Pulse2, color = Ran)) +
+
+ggplot(pulse_data,
+       mapping = aes(x = Pulse1, y = Pulse2, color = RanFct)) +
   geom_point() +
   geom_smooth(method = "lm") +
   coord_cartesian(xlim = c(40, 180), ylim = c(40, 180)) +
-  facet_wrap(~ Smokes) +
+  facet_wrap(~ SmokesFct) +
   theme_classic()
+
+pulse_data |>
+  filter(SmokesFct == "No" & RanFct == "Ran")
+
+pulse_data |>
+  filter(SmokesFct == "No") |>
+  filter(RanFct == "Ran")
+
+
 
 
 
